53c6962db9f63d21651f55668526ff5e046d3677
Jar <114672583+ddys001@users.noreply.github.com>
2023-04-04 15:08:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 echo "阿里云盘Token一键搭建脚本"
-echo "脚本作者：舒夏"
-echo "GitHub开源地址：https://github.com/sxbai/alitoken-on-replit"
+echo "脚本作者：豆豆"
+echo "GitHub开源地址：https://github.com/ddys001/alitoken"
 nix-env -iA nixpkgs.wget
-wget -O main.sh https://github.com/sxbai/alitoken-on-replit/raw/master/main.sh
+wget -O main.sh https://github.com/ddys001/alitoken/raw/main/main.sh
 mkdir build
 cd build
-wget -O .replit https://github.com/sxbai/nginx-on-replit/raw/master/.replit
+wget -O .replit https://github.com/ddys001/alitoken/raw/main/.replit
 cd ..
 cp -r build/.replit . && rm -rf build/
-wget -O ali.zip https://github.com/sxbai/alitoken-on-replit/raw/master/ali.zip
+wget -O ali.zip https://github.com/ddys001/alitoken/raw/main/ali.zip
 nix-env -iA nixpkgs.unzip
 unzip ali.zip
 rm -rf ali.zip
 cp -r a/.cache .cache
 rm -rf a/
 echo "阿里云盘Token一键搭建脚本"
-echo "脚本作者：舒夏"
-echo "GitHub开源地址：https://github.com/sxbai/alitoken-on-replit"
+echo "脚本作者：豆豆"
+echo "GitHub开源地址：https://github.com/ddys001/alitoken"
 echo "token.txt填入你的32位阿里云盘Token"
-echo "脚本作者：舒夏"
+echo "脚本作者：豆豆"
 echo "点击Run按钮，启动项目! 使用愉快!!!"
 rm -rf README.md
